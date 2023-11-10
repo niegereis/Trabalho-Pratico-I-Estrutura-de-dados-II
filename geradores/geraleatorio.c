3255222a2eb38e98fc34024da3059ba43ea18a52
Dudu <eduardogps202@gmail.com>
2023-11-10 17:37:12 -0300

    FILES CREATE

--- geradores/geraleatorio.c
+++ geradores/geraleatorio.c
@@ -1,48 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define TAM 2000000
+#define TAM 10
+#define LONG_MAX 10
 
 void embaralha(int*);
 
-typedef struct registro{
-    int chave;
-    long dado1;
-    char dado2[5000];
-    
-}Registro;
+typedef struct registro {
+  int chave;
+  long dado1;
+  char dado2[5000];
 
-int main (){
-    int vetor[TAM];
-    srand(42);
-    Registro registro;
-    FILE* arquivo = fopen ("aleatorio.bin", "wb");
+} Registro;
 
-    for(int i = 0; i<TAM; i++){
-        vetor[i] = i;
+int main() {
+  int vetor[TAM];
+  srand(42);
+  Registro registro;
+  FILE* arquivo = fopen("aleatorio.bin", "wb");
+
+  for (int i = 0; i < TAM; i++) {
+    vetor[i] = i;
+  }
+  embaralha(vetor);
+
+  for (int i = 0; i < TAM; i++) {
+    registro.chave = vetor[i];
+    registro.dado1 = rand() % LONG_MAX;
+    for (int j = 0; j < 4999; j++) {
+      registro.dado2[j] = rand() % 93 + 33;
     }
-    embaralha(vetor);
+    registro.dado2[4999] = 'A\0';
+    fwrite(&registro, sizeof(Registro), 1, arquivo);
+  }
 
-    for(int i = 0; i < TAM; i++){
-        registro.chave = vetor[i];
-        registro.dado1 = rand() % LONG_MAX;
-        for(int j = 0; j < 4999; j++){
-            registro.dado2[j] = rand() % 93 + 33;
-        }
-        registro.dado2[4999] = '\0';
-        fwrite(&registro, sizeof(Registro), 1, arquivo);
-    }
-    fclose(arquivo);
-    return 0;
+  fseek(arquivo, 0, 0);
+  while (fread(&registro, sizeof(Registro), TAM, arquivo) == 1) {
+    printf("-> %d %d %s", registro.chave, registro.dado1, registro.dado2);
+  }
+  fclose(arquivo);
+  return 0;
 }
 
 void embaralha(int* vetor) {
-    for (int i = TAM - 1; i > TAM; i--) {
-        int posicaoAleatoria = rand() % (i + 1);
-        
-        int temp = vetor[i];
-        vetor[i] = vetor[posicaoAleatoria];
-        vetor[posicaoAleatoria] = temp;
-    }
-    return vetor;
+  for (int i = TAM - 1; i > TAM; i--) {
+    int posicaoAleatoria = rand() % (i + 1);
+
+    int temp = vetor[i];
+    vetor[i] = vetor[posicaoAleatoria];
+    vetor[posicaoAleatoria] = temp;
+  }
+  return vetor;
 }
--- geradores/gercrescente.c
+++ geradores/gercrescente.c
@@ -1,30 +1,39 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<limits.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define TAM 2000000
+#define TAM 10
 
-typedef struct registro{
-    int chave;
-    long dado1;
-    char dado2[5000];
-    
-}Registro;
+typedef struct registro {
+  int chave;
+  long dado1;
+  char dado2[5000];
 
-int main (){
-    srand(time(NULL));
-    Registro registro;
-    FILE* arquivo = fopen ("crescendente.bin", "wb");
-    for(int i = 0; i < TAM; i++){
-        registro.chave = i;
-        registro.dado1 = rand() % LONG_MAX;
-        for(int j = 0; j < 4999; j++){
-            registro.dado2[j] = rand() % 93 + 33;
-        }
-        registro.dado2[4999] = '\0';
-        fwrite(&registro, sizeof(Registro), 1, arquivo);
+} Registro;
+
+int main() {
+  srand(time(NULL));
+  Registro registro;
+  FILE* arquivo = fopen("crescendente.bin", "wb+");
+  for (int i = 0; i < TAM; i++) {
+    registro.chave = i;
+    registro.dado1 = rand() % LONG_MAX;
+    for (int j = 0; j < 4999; j++) {
+      registro.dado2[j] = rand() % 93 + 33;
     }
-    fclose(arquivo);
-    return 0;
+    registro.dado2[0] = 'a';
+    registro.dado2[1] = '\0';
+    // registro.dado2[4999] = '\0';
+    fwrite(&registro, sizeof(Registro), 1, arquivo);
+  }
+
+  printf("AQ\n");
+  fseek(arquivo, 0, SEEK_SET);
+  while (fread(&registro, sizeof(Registro), 1, arquivo) == 1) {
+    printf("-> %d %ld %s\n", registro.chave, registro.dado1, registro.dado2);
+  }
+
+  fclose(arquivo);
+  return 0;
 }
