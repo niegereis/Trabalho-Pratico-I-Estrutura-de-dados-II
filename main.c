65a8d024b1ef1ea7643a4521ea2b52df10e876cb
Cecilia <ceciliaperet81@gmail.com>
2023-11-14 23:40:25 -0300

    Mudando o main

--- main.c
+++ main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main(int argc, char *argv[]){
 
     int metodo;
     int situacao;
     int chave;
     int quantidade;
+
+    quantidade = atoi(argv[2]);
+    metodo = atoi(argv[3]);
+    chave = atoi(argv[4]);
+    metodo = atoi(argv[0]);
+    
 
 
     return 0;
