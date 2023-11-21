25b4a9e808b23036950365f59d44e285e11d051f
Niege Reis <niegemr@gmail.com>
2023-11-21 18:25:40 -0300

    fix issues arvoreB

--- project/lib/arvoreB.h
+++ project/lib/arvoreB.h
@@ -8,25 +8,25 @@
 typedef struct TipoPagina* TipoApontador;
 
 typedef struct TipoPagina {
-    short n;
-    Registro r[2*M];
-    TipoApontador p[2*M + 1]; // vetor de ponteiros que vai apontar para as proximas paginas
+  short n;
+  Registro r[2 * M];
+  TipoApontador p[2 * M + 1];  // vetor de ponteiros que vai apontar para as
+                               // proximas paginas
 } TipoPagina;
-
 
 bool Pesquisa(Registro*, TipoApontador, Analise*);
 
 void Imprime(TipoApontador);
 
-void InsereNaPagina (TipoApontador, Registro, TipoApontador, Analise*);
+void InsereNaPagina(TipoApontador, Registro, TipoApontador, Analise*);
 
-void Ins (Registro, TipoApontador, short*, Registro*, TipoApontador*, Analise*);
+void Ins(Registro, TipoApontador, short*, Registro*, TipoApontador*, Analise*);
 
 void Insere(Registro, TipoApontador*, Analise*);
 
-//void arvoreB(Analise*, FILE*, Registro *, int , int, TipoApontador);
+// void arvoreB(Analise*, FILE*, Registro *, int , int, TipoApontador);
 
-void InicializaArvoreB (TipoApontador);
+void InicializaArvoreB(TipoApontador*);
 
 void LiberaPagina(TipoApontador);
 
